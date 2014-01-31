19527a3c658f4602d57a905860c4fd779f8638fe
Nantas Nardelli <nantas.nardelli@gmail.com>
2014-01-31 22:17:21 +0000

    Restored initial configuration

--- elpa/popup-20130708.2245/popup-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; popup-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("popup-pkg.el" "popup.el") (21073 23101
-;;;;;;  234779 899000))
-
-;;;***
-
-(provide 'popup-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; popup-autoloads.el ends here
--- elpa/seclusion-mode-1.1.1/seclusion-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "seclusion-mode" "1.1.1" "Edit in seclusion. A Dark Room mode." (quote nil))
