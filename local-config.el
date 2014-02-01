fb54b7fcaa57771ce9e02379640612262ba7d582
Nantas Nardelli <nantas.nardelli@gmail.com>
2014-02-01 14:10:09 +0000

    Add solarized theme

--- local-config.el
+++ local-config.el
@@ -1,5 +1,8 @@
 ;; Loaded from init file 
 
+;; Theme loading
+(load "~/.emacs.d/vendor/themes/theme.el")
+
 ;; Let's use some global variables
 (setq vendor-path "~/.emacs.d/vendor/")
 (setq config-path "~/.emacs.d/config/")
@@ -7,3 +10,4 @@
 (load "~/.emacs.d/config/general.el")
 (load "~/.emacs.d/config/look.el")
 (load "~/.emacs.d/config/navigation.el")
+
---
+++ vendor/themes/theme.el
@@ -0,0 +1,3 @@
+(add-to-list 'custom-theme-load-path "~/.emacs.d/vendor/themes/solarized")
+;; (load-theme 'solarized-light t)
+(load-theme 'solarized-dark t)
