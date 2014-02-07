934868f8a770f079a73b98fef8b3d837fc2e4bb8
Nantas Nardelli <nantas.nardelli@gmail.com>
2014-02-07 16:58:26 +0000

    Fix flycheck lisp doc warnings

--- config/flycheck.el
+++ config/flycheck.el
@@ -1,9 +1,9 @@
-;; [==:INIT flycheck==]
 (add-hook 'after-init-hook 'global-flycheck-mode)
 ;; (setq flycheck-check-syntax-automatically '(mode-enabled idle-change))
 ;; 
 (require 'flycheck-color-mode-line)
-;; (eval-after-load "flycheck"
-;;  '(add-hook 'flycheck-mode-hook 'flycheck-color-mode-line-mode))
+(eval-after-load "flycheck"
+  '(add-hook 'flycheck-mode-hook 'flycheck-color-mode-line-mode))
 
-(eval-after-load 'flycheck (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc)))
+;; Disable annoying documentation warnings for lisp code
+(setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc))
