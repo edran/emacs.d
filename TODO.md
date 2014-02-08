918dee42b52f78c0074e28c6d0f88efe0c7b6e21
Nantas Nardelli <nantas.nardelli@gmail.com>
2014-02-08 14:41:59 +0000

    Add TODOlist, tramp mode and some more tweaks

---
+++ TODO.md
@@ -0,0 +1,6 @@
+TODO LIST
+=========
+
+* Yasnippet or auto-insert-mode or defaultcontent.el for templates (c, python, haskell)
+
+* Thesaurus for synonyms 
---
+++ config/tramp-mode.el
@@ -0,0 +1,13 @@
+;; TRAMP stands for `Transparent Remote (file) Access, Multiple
+;; Protocol'. This package provides remote file editing, similar to
+;; Ange-FTP. The difference is that Ange-FTP uses FTP to transfer
+;; files between the local and the remote host, whereas TRAMP uses a
+;; combination of rsh and rcp or other work-alike programs, such as
+;; ssh/scp.
+
+;; /server.com:/home/edran/public/foo.html
+
+
+(require 'tramp)
+(setq tramp-default-method "ssh")
+
