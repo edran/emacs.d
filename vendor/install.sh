bcc22885eb3c5dd13a2d6f6bd0e3a82b3e91ad76
Nantas Nardelli <nantas.nardelli@gmail.com>
2014-02-13 11:06:56 +0000

    Using only elpa packages now

--- vendor/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Run just after having cloned the repository
-
-echo "#################### Haskell Mode is compiling ####################"
-cd haskell-mode
-make all
-cd ..
-echo "####################     DONE:Haskell Mode     ####################"
