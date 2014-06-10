2bbe97752bc67227fe714be899e355562e2fdc8a
Nantas Nardelli <nantas.nardelli@gmail.com>
2014-06-10 15:19:51 +0100

    Add window-changing keys

--- config/navigation.el
+++ config/navigation.el
@@ -85,3 +85,8 @@
 
 (global-set-key (kbd "C-x 2") 'vsplit-last-buffer)
 (global-set-key (kbd "C-x 3") 'hsplit-last-buffer)
+
+(global-set-key (kbd "S-C-<left>") 'shrink-window-horizontally)
+(global-set-key (kbd "S-C-<right>") 'enlarge-window-horizontally)
+(global-set-key (kbd "S-C-<down>") 'shrink-window)
+(global-set-key (kbd "S-C-<up>") 'enlarge-window)
