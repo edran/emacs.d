ccd89e23c3c76b3f5f62dc4df5404c4a94c3876e
Nantas Nardelli <nantas.nardelli@gmail.com>
2014-11-04 14:00:12 +0000

    Remove todo and fix conflict with eshell and god-mode

--- lisp/config/god-mode.el
+++ lisp/config/god-mode.el
@@ -36,6 +36,9 @@
 (define-key god-local-mode-map (kbd "z") 'repeat)
 (define-key god-local-mode-map (kbd "i") 'god-local-mode)
 
+(add-to-list 'god-exempt-major-modes 'eshell-mode)
+
+
 ;; (global-set-key (kbd "C-x C-1") 'delete-other-windows)
 ;; (global-set-key (kbd "C-x C-2") 'split-window-below)
 ;; (global-set-key (kbd "C-x C-3") 'split-window-right)
