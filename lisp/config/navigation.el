7b70146f6a9d3b4d5bce8478157ff5605c5b5235
Nantas Nardelli <nantas.nardelli@gmail.com>
2015-01-20 08:01:27 +0000

    Make isearch-fwd behave like isearch backward

--- lisp/config/navigation.el
+++ lisp/config/navigation.el
@@ -27,20 +27,21 @@
 ;; (require 'smooth-scrolling)
 ;; (require 'smooth-scroll)
 ;; (setq scroll-step 1)
-    (setq scroll-conservatively 10000)
+(setq scroll-conservatively 10000)
 ;;    (setq auto-window-vscroll nil)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; isearch  made better
 
-;; (add-hook 'isearch-mode-end-hook 'my-goto-match-beginning)
-;; (defun my-goto-match-beginning ()
-;;   (when (and isearch-forward (not isearch-mode-end-hook-quit))
-;;     (goto-char isearch-other-end))
-;; )
+(add-hook 'isearch-mode-end-hook 'my-goto-match-beginning)
+(defun my-goto-match-beginning ()
+      (when (and isearch-forward isearch-other-end
+                 (not isearch-mode-end-hook-quit))
+        (goto-char isearch-other-end)))
 
-;;  (defun my-goto-match-beginning ()
-;;    (when isearch-forward (goto-char isearch-other-end)))
-
+(defadvice isearch-exit (after my-goto-match-beginning activate)
+      "Go to beginning of match."
+      (when (and isearch-forward isearch-other-end)
+        (goto-char isearch-other-end)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Keyset for switching between frames
