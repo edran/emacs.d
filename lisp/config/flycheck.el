a59c60f0c1f8fb54565f958c5f08e46bd6069b1e
Nantas Nardelli <nantas.nardelli@gmail.com>
2015-02-04 14:42:00 +0000

    Clean up flycheck config

--- lisp/config/flycheck.el
+++ lisp/config/flycheck.el
@@ -1,36 +1,19 @@
 (require 'flycheck)
 
-(setq-default flycheck-emacs-lisp-load-path 'inherit)
-(setq flycheck-check-syntax-automatically '(save
-                                            ;; idle-change ; I save a
-                                            ;; lot anyway
-                                            mode-enabled))
+;; basic config
 
-(defun set-flycheck-auto-speed ()
+(setq flycheck-check-syntax-automatically '(save new-line mode-enabled)
+      flycheck-highlighting-mode 'symbols)
+
+(defun me/set-flycheck-auto-speed ()
   (setq flycheck-idle-change-delay
         (if flycheck-current-errors 2.0 20.0)))
-
 ;; Each buffer gets its own idle-change-delay because of the
 ;; buffer-sensitive adjustment above.
 (make-variable-buffer-local 'flycheck-idle-change-delay)
 
 (add-hook 'flycheck-after-syntax-check-hook
-          'set-flycheck-auto-speed)
-
-;; Disable annoying documentation warnings for lisp code
-(setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc))
-
-;; run pylint --rcfile=~/.emacs.d/config/pylint.conf
-
-(add-hook 'c++-mode-hook (lambda ()
-                           (setq flycheck-clang-language-standard "c++11")))
-
-;; (setq-default flycheck-clang-language-standard "c")
-
-(setq flycheck-highlighting-mode 'symbols)
-(eval-after-load 'flycheck
-  '(add-hook 'flycheck-mode-hook #'flycheck-haskell-setup))
-
+          'me/set-flycheck-auto-speed)
 (add-hook 'prog-mode-hook 'flycheck-mode)
 
 ;; This got annoying quite quickly
@@ -38,3 +21,23 @@
 ;; (eval-after-load 'flycheck
 ;;   '(custom-set-variables
 ;;    '(flycheck-display-errors-function #'flycheck-pos-tip-error-messages)))
+
+
+;; ######## elisp
+
+;; Disable annoying documentation warnings for lisp code
+(setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc))
+(setq-default flycheck-emacs-lisp-load-path 'inherit)
+
+
+;; ######## C/C++
+
+;; (setq-default flycheck-clang-language-standard "c")
+(add-hook 'c++-mode-hook (lambda ()
+                           (setq flycheck-clang-language-standard "c++11")))
+
+
+;; ######## Haskell
+
+(eval-after-load 'flycheck
+  '(add-hook 'flycheck-mode-hook #'flycheck-haskell-setup))
