b88de5bdcf0e5997d964e59d763a12d37c7b837a
Nantas Nardelli <nantas.nardelli@gmail.com>
2015-02-04 19:25:52 +0000

    Clean up cc-mode

--- lisp/config/cc-mode.el
+++ lisp/config/cc-mode.el
@@ -1,13 +1,20 @@
 (require 'cc-mode)
-(setq-default c-basic-offset 2 c-default-style "google")
+(setq-default
+ c-basic-offset 2
+ c-default-style "google")
 
 (require 'google-c-style)
 (add-hook 'c-mode-common-hook 'google-set-c-style)
 (add-hook 'c-mode-common-hook 'google-make-newline-indent)
 (require 'cmake-project)
 
-; Add cmake listfile names to the mode list.
-(require 'cmake-project)
+;; Sigh.
+(add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
+(add-hook 'c++-mode-hook
+          (lambda () (setq comment-start "/* " comment-end " */")))
+
+
+;; ########### CMAKE fixes
 
 (defun maybe-cmake-project-hook ()
   (if (file-exists-p "CMakeLists.txt") (cmake-project-mode)))
@@ -20,27 +27,17 @@
                   ("\\.cmake\\'" . cmake-mode))
                 auto-mode-alist))
 
-;; Sigh.
-(add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
-(add-hook 'c++-mode-hook
-          (lambda () (setq comment-start "/* " comment-end " */")))
 
-
+;; ########### cpputils
 (require 'cpputils-cmake)
 (add-hook 'c-mode-common-hook
           (lambda ()
             (if (derived-mode-p 'c-mode 'c++-mode)
                 (cppcm-reload-all)
               )))
-;; OPTIONAL, somebody reported that they can use this package with Fortran
-(add-hook 'c90-mode-hook (lambda () (cppcm-reload-all)))
-;; OPTIONAL, avoid typing full path when starting gdb
 (global-set-key (kbd "C-c C-g")
  '(lambda ()(interactive) (gud-gdb (concat "gdb --fullname " (cppcm-get-exe-path-current-buffer)))))
-;; OPTIONAL, some users need specify extra flags forwarded to compiler
-
 (setq cppcm-extra-preprocss-flags-from-user '("-I/usr/src/linux/include"
                                               "-DNDEBUG"
                                               "-I/opt/ros/hydro/include"))
-
 (setq cppcm-write-flymake-makefile nil)
