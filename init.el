f51a5463524aaf2affcc7e6fb5f3fb51823bc662
Nantas Nardelli <nantas.nardelli@gmail.com>
2015-09-18 18:32:19 +0100

    Clean up init

--- init.el
+++ init.el
@@ -1,10 +1,22 @@
+;; Use common lisp
 (with-no-warnings
   (require 'cl-lib))
-;; (server-start)
 
-(let ((default-directory "~/.emacs.d/vendor/"))
+;; Define some configuration variables
+(defvar config-path
+  (file-name-as-directory (concat user-emacs-directory "lisp")))
+(defvar third-party-path
+  (file-name-as-directory (concat user-emacs-directory "vendor")))
+(defvar local-config-file
+  (concat config-path "local-config.el"))
+
+;; Add third party packages to load path
+(let ((default-directory third-party-path))
  (normal-top-level-add-subdirs-to-load-path))
 
-(setq custom-file "~/.emacs.d/lisp/customs.el")
+;; Keep custom config in separate file
+(setq custom-file
+      (expand-file-name "lisp/customs.el" user-emacs-directory))
 (load custom-file)
-(load  "~/.emacs.d/lisp/local-config.el")
+
+(load local-config-file)
